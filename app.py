4e2c48d316e77f05542e855f6667766c2c464a7d
Flavio Soares <nerdmaluk@gmail.com>
2024-05-23 17:37:02 -0100

    Alteração2.txt

---
+++ app.py
@@ -0,0 +1,9 @@
+# Python program for simple calculator
+
+# Function to add two numbers
+def add(num1, num2):
+	return num1 + num2
+
+# Function to subtract two numbers
+def subtract(num1, num2):
+	return num1 - num2
