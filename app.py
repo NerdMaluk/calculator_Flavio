1aa1c9b4b532771d6ca397bfb4814e36452cd6b6
Flavio Soares <nerdmaluk@gmail.com>
2024-05-23 18:10:35 -0100

    alteracao3

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ def add(num1, num2):
 # Function to subtract two numbers
 def subtract(num1, num2):
 	return num1 - num2
+# Function to multiply two numbers
+def multiply(num1, num2):
+	return num1 * num2
