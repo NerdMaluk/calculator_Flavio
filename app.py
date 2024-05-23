22ba4f1dc2f2001d876c99294d87bc0cc9abe823
Flavio Soares <nerdmaluk@gmail.com>
2024-05-23 18:19:21 -0100

    alteracao5

--- app.py
+++ app.py
@@ -7,3 +7,15 @@ def add(num1, num2):
 # Function to subtract two numbers
 def subtract(num1, num2):
 	return num1 - num2
+print("Please select operation -\n" \
+		"1. Add\n" \
+		"2. Subtract\n" \
+		"3. Multiply\n" \
+		"4. Divide\n")
+
+
+# Take input from the user
+select = int(input("Select operations form 1, 2, 3, 4 :"))
+
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
